6a68ba98c931cb2e6c6dc857b95ffa3b54b37c30
Jörgen Nilsson [MVP] <jorgen@ccmexec.com>
2024-02-08 10:33:35 +0100

    Add files via upload

---
+++ CheckSelfDeploying.ps1
@@ -0,0 +1,22 @@
+# Script to check if the device is deployad as a selfe-deploying device
+# Written by Jörgen Nilsson
+# ccmexec.com
+function Get-EnrolledUser {
+    # Get the UPN of the user that enrolled the computer to AAD
+    $AADInfo = Get-Item "HKLM:/SYSTEM/CurrentControlSet/Control/CloudDomainJoin/JoinInfo"
+
+    $guids = $AADInfo.GetSubKeyNames()
+    foreach ($guid in $guids) {
+        $guidSubKey = $AADinfo.OpenSubKey($guid);
+        $UPN = $guidSubKey.GetValue("UserEmail");
+    }
+    $UserName = ($UPN -split ("@"))[0]
+    Write-Output $UserName
+}
+
+if (Get-EnrolledUser -eq "autopilot") {
+    return $true
+}
+else {
+    return $false
+}
